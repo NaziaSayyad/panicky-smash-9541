58dc9bc7ff5de7a6b7a8d9e811bddd0017c6fcde
Nazia Sayyad <sayyadnazia257@gmai.com>
2022-08-28 16:53:59 +0530

    done

--- frontend/export.js
+++
@@ -1,7 +0,0 @@
-import footer from "./components/footer.js";
-console.log(footer())
-document.getElementById("footer_import").innerHTML=footer()
-
-import navbar from "./components/navbar.js";
-console.log(navbar())
-document.getElementById("navbar_import").innerHTML=navbar()
